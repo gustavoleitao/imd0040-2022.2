7e036cce66ccda2dd3e05545fa86a11598528001
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2022-09-01 22:00:15 -0300

    inicial commit

---
+++ aula01/Main.java
@@ -0,0 +1,11 @@
+class Main {
+
+    public static void main(String[] args){
+        System.out.println("Começando...");
+        for (String arg: args){
+            System.out.println(arg);
+        }
+        System.out.println("Fim!");
+    }
+
+}
